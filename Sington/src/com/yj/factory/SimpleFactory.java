53efc415c73d369e2c2dd956175d047bdfabd321
wo243 <wo243@LAPTOP-KB9746AD>
2018-03-29 00:34:37 +0800

    简单工厂模式

---
+++ Sington/src/com/yj/factory/SimpleFactory.java
@@ -0,0 +1,5 @@
+package com.yj.factory;
+
+public class SimpleFactory {
+
+}
--- Sington/src/com/yj/reflect/Reflection.java
+++ Sington/src/com/yj/reflect/Reflection.java
@@ -2,6 +2,6 @@ package com.yj.reflect;
 
 public class Reflection {
     public static void main(String args[]){
-        System.out.print();
+        System.out.print("##########测试开始#########");
     }
 }
